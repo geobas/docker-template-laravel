08eed26fe7da16c730efd299d7f2b105cc713a76
geobas <ksenera@yahoo.com>
2022-02-02 23:00:43 +0200

    minor change

--- install.sh
+++ install.sh
@@ -3,7 +3,7 @@
 echo "Which framework you wish to install?"
 select pckg in "Laravel" "Lumen"; do
     case $pckg in
-        Laravel ) composer create-project --prefer-dist laravel/laravel temp && shopt -s dotglob nullglob && mv temp/* . && rmdir temp; break;;
-        Lumen ) composer create-project --prefer-dist laravel/lumen temp && shopt -s dotglob nullglob && mv temp/* . && rmdir temp; break;;
+        Laravel ) composer create-project --prefer-dist laravel/laravel temp && shopt -s dotglob nullglob && mv temp/* . && rmdir temp && echo && ./artisan --version; break;;
+        Lumen ) composer create-project --prefer-dist laravel/lumen temp && shopt -s dotglob nullglob && mv temp/* . && rmdir temp && echo && ./artisan --version; break;;
     esac
 done
